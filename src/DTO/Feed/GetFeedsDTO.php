a39185335a02f4f842bcf009a73ad99acd7215af
Olivier <olersteau@gmail.com>
2025-10-17 17:24:22 +0200

    Create DTO for request with custom validators

---
+++ src/DTO/Feed/GetFeedsDTO.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DTO\Feed;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Validator\ValidFeedSource;
+use App\Validator\ValidDateString;
+
+
+final class GetFeedsDTO
+{
+    #[Assert\Type('numeric', message: 'El valor "{{ value }}" no es válido para el campo límite.')]
+    #[Assert\Positive(message: 'El límite debe ser un número positivo')]
+    public ?string $sqlResult = null;
+
+    #[ValidFeedSource]
+    public ?string $source = null;
+
+    #[ValidDateString]
+    public ?string $since = null;
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return is_null($this->since) ? null : new \DateTime($this->since);
+    }
+
+    public function getLimit(): ?int
+    {
+        return is_null($this->sqlResult) ? null : (int) $this->sqlResult;
+    }
+
+    public function getSource(): ?string
+    {
+        return $this->source;
+    }
+}
